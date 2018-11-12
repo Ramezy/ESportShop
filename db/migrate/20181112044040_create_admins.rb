b5a1fedc8170df9bd6e4dc746f3f12f36fabfb11
Aramide <aramideakanbi@gmail.com>
2018-11-11 22:41:21 -0600

    Creation of admin model

---
+++ db/migrate/20181112044040_create_admins.rb
@@ -0,0 +1,10 @@
+class CreateAdmins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admins do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
