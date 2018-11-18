537cecbeddc721fe963a4f79688179305fb4c02a
Aramide <aramideakanbi@gmail.com>
2018-11-17 21:59:22 -0600

    Implemented index and show product controller actions

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,7 +1,10 @@
 class ProductController < ApplicationController
   def index
+    @product_collection = Product.order(:name)
   end
 
+  # GET /product/:id
   def show
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'product/index'
-  get 'product/show'
   get 'pages/permalink'
   get 'pages/show'
   get 'pages/about'
-  get 'home/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :product, only: [:index, :show]
+  resources :home, only:[:index]
+  root to: 'home#index'
+
 end
