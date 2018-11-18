5a0bdad5b5cbd200e0c25f052c019ac873b44a5b
Aramide <aramideakanbi@gmail.com>
2018-11-17 19:35:15 -0600

    Created the category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_190412) do
+ActiveRecord::Schema.define(version: 2018_11_18_012309) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(version: 2018_11_16_190412) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "customers", force: :cascade do |t|
