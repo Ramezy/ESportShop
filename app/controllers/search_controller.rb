867fc5c6b8d8b9d68323acb64a51451472941b71
Aramide <aramideakanbi@gmail.com>
2018-11-21 10:42:15 -0600

    improved the partial with images

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
   end
 
-  def result
-    @products = Product.where('productName LIKE ?', "%#{params[:q]}%").page(params[:page])
+  def results
+    @products = Product.where('productName LIKE ?', "%#{params[:q]}%").where('productStyle LIKE ?', "%#{params[:q]}%").page(params[:page])
   end
 end
