46ce007d98ff57183892cf90d4cdc8e3c7b4e2ee
Aramide <aramideakanbi@gmail.com>
2018-11-30 11:19:47 -0600

    Added checkout

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -41,7 +41,7 @@ class CartController < ApplicationController
       if @qty > @pro_in_stock.stock
         @error = "Only #{@pro_in_stock.stock} in stock !"
 
-        return redirect_to showcart_path(:error => @error)
+        return redirect_to cart_show_path(:error => @error)
       else
         @pro_in_stock.stock = @pro_in_stock.stock - @qty
 
@@ -50,13 +50,68 @@ class CartController < ApplicationController
         end
 
         @pro_in_stock.save
+
         @products << params["product_id_#{i}".to_sym].to_i
         @quantities << params["quantity_#{i}".to_sym].to_i
       end
+    end
+
+      @gst_value = 1
+      if current_user.province.gst.present?
+        @gst_value = current_user.province.gst
+        @gst_value = @total.to_d * @gst_value/100
+      end
+
+      @pst_value = 1
+      if current_user.province.pst.present?
+        @pst_value = current_user.province.pst
+        @pst_value = @total.to_d * @pst_value/100
+      end
+
+      @hst_value = 1
+      if current_user.province.hst.present?
+        @hst_value = current_user.province.hst
+        @hst_value = @total.to_d * @hst_value/100
+      end
+
+      @taxes = 0
+      @tax_info = "Taxes Charged According to " + current_user.province.name.to_s + "."
+
+      if @gst_value == 1
+        @taxes += 0
+      else
+        @taxes += @gst_value
+        @gst_value = helper.number_to_currency(@gst_value)
+        @tax_info += "\n" + "GST Charged: " + @gst_value.to_s + "at" + current_user.province.gst.to_s + "%."
+      end
+
+      if @pst_value == 1
+        @taxes += 0
+      else
+        @taxes += @pst_value
+        @pst_value = helper.number_to_currency(@pst_value)
+        @tax_info += "\n" + "PST Charged: " + @pst_value.to_s + "at" + current_user.province.pst.to_s + "%."
+      end
+
+      if @hst_value == 1
+        @taxes += 0
+      else
+        @taxes += @hst_value
+        @hst_value = helper.number_to_currency(@hst_value)
+        @tax_info += "\n" + "HST Charged: " + @hst_value.to_s + "at" + current_user.province.hst.to_s + "%."
+      end
+
+      @grand_total = @total + @taxes
+
+      order = Order.create(quantity: number_of_products, user_id: current_user.id, order_status_id: 1, total: @total, taxes: @taxes, tax_info: @tax_info, grand_total: @grand_total)
+
+      number_of_products.times do |k|
+        sold_price = Product.find(@products[k]).productCost
+        ProductOrder.create(quantity: @quantities[k], total: sold_price, product_id: @products[k], order: order)
+      end
 
       session[:pro_cart] = []
-      @order_last = Order.all
-    end
+      @order_last = order
   end
 
    
--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,39 @@
 class ChargesController < ApplicationController
+    def new
+        session[:order_id] = params[:id].to_i
+        @amount = Order.find(params[:id]).grand_total
+        @amount = @amount * 100
+        session[:pro_cent_value] = @amount.to_i
+    end
+
+    def create
+        @amount_pro = session[:pro_cent_value]
+
+        customer = Stripe::Customer.create(
+            :email => params[:stripeEmail],
+            :source => params[:stripeToken]
+        )
+
+        charge = Stripe::Charge.create(
+            :customer => customer.id,
+            :amount => @amount_pro,
+            :description => 'Rails Stripe customer'
+            :currency => 'usd'
+        )
+
+        if charge.paid && charge.amount == @amount_pro
+            @order_id = session[:order_id].to_i
+
+            order = Order.find(@order_id)
+
+            order.charge_id = charge_id
+            order.order_status_id = 2
+
+            order.save
+        end
+
+    rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_charge_path
+    end
 end
